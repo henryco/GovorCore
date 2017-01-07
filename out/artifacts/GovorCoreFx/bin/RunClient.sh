cb3cbec314e4a256c7d99f2b798e3b34fba361b3
HenryCo <hd2files@gmail.com>
2017-01-07 01:32:11 +0100

    out

---
+++ out/artifacts/GovorCoreFx/bin/RunClient.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#@author HenryCo 
+#github { https://github.com/henryco }
+#contact {hd2files@google_mail}
+
+#linux chat client start script
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+"$DIR"/./GovorCore.jar "$DIR"/ client # --> client args here <-- #
---
+++ out/artifacts/GovorCoreFx/client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/root/GovorCore/out/artifacts/GovorCoreFx/bin/./RunClient.sh
